06257088012c890aecd00200c17f9098a648f23b
João Guilherme <jgwaquim@hotmail.com>
2020-05-08 16:26:18 -0300

    Params bug fixed, routes fixed and new operation link

--- app/controllers/calculo_controller.rb
+++ app/controllers/calculo_controller.rb
@@ -3,26 +3,26 @@ class CalculoController < ApplicationController
     end
 
     def soma
-        @num1 = params[:x]
-        @num2 = params[:y]
+        @num1 = params[:num1]
+        @num2 = params[:num2]
         @resultado = @num1.to_f + @num2.to_f
     end
 
     def subtracao
-        @num1 = params[:x]
-        @num2 = params[:y]
+        @num1 = params[:num1]
+        @num2 = params[:num2]
         @resultado = @num1.to_f - @num2.to_f
     end
 
     def multiplicacao
-        @num1 = params[:x]
-        @num2 = params[:y]
+        @num1 = params[:num1]
+        @num2 = params[:num2]
         @resultado = @num1.to_f * @num2.to_f
     end
 
     def divisao
-        @num1 = params[:x]
-        @num2 = params[:y]
+        @num1 = params[:num1]
+        @num2 = params[:num2]
         @resultado = @num1.to_f / @num2.to_f
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'calculo#index'
+  get '/index' => 'calculo#index'
   post '/soma' => 'calculo#soma'
   post '/subtracao' => 'calculo#subtracao'
   post '/divisao' => 'calculo#divisao'
