a86352b1e5455612a7a642b7d4e08fc3551b3111
João Guilherme <jgwaquim@hotmail.com>
2020-05-14 18:44:20 -0300

    Added before_action parameters instantiation to simplify the code

--- app/controllers/calculo_controller.rb
+++ app/controllers/calculo_controller.rb
@@ -1,28 +1,24 @@
 class CalculoController < ApplicationController
+    before_action do
+        @num1 = params[:num1.to_f]
+        @num2 = params[:num2.to_f]
+    end
     def index
     end
 
     def soma
-        @num1 = params[:num1]
-        @num2 = params[:num2]
         @resultado = @num1.to_f + @num2.to_f
     end
 
     def subtracao
-        @num1 = params[:num1]
-        @num2 = params[:num2]
         @resultado = @num1.to_f - @num2.to_f
     end
 
     def multiplicacao
-        @num1 = params[:num1]
-        @num2 = params[:num2]
         @resultado = @num1.to_f * @num2.to_f
     end
 
     def divisao
-        @num1 = params[:num1]
-        @num2 = params[:num2]
         @resultado = @num1.to_f / @num2.to_f
     end
 
